f80676b181cd430d6699634af3a01f761580b81e
StefanVinica <stefannikolovski996@gmail.com>
2023-11-27 19:05:24 +0100

    dynamic port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+
 
 // Middleware to parse JSON body
 app.use(express.json());
@@ -30,7 +30,7 @@ app.post('/send-data', async (req, res) => {
   }
 });
 
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://:${port}`);
 });

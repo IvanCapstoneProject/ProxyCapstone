87d56f674b85a8081071439209ded034461e6d33
StefanVinica <stefannikolovski996@gmail.com>
2023-11-27 18:50:27 +0100

    port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 // Middleware to parse JSON body
 app.use(express.json());
